b6d9e1d037550d9051047eeac0e73b8d27589c91
BalderNordmann <zschornack.max@gmail.com>
2025-12-09 17:15:08 +0100

    Add BookItem

---
+++ GrpcDemo/GrpcClient/Themes/UserControls/BookItem.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GrpcClient.Themes.UserControls
+{
+    /// <summary>
+    /// Interaction logic for BookItem.xaml
+    /// </summary>
+    public partial class BookItem : UserControl
+    {
+        public BookItem()
+        {
+            InitializeComponent();
+        }
+
+        public string BookTitle
+        {
+            get => (string)GetValue(BookTitleProperty);
+            set => SetValue(BookTitleProperty, value);
+        }
+
+        public static readonly DependencyProperty BookTitleProperty =
+            DependencyProperty.Register(nameof(BookTitle), typeof(string), typeof(ShoppingCartItem));
+
+        public string BookAuthor
+        {
+            get => (string)GetValue(BookAuthorProperty);
+            set => SetValue(BookAuthorProperty, value);
+        }
+
+        public static readonly DependencyProperty BookAuthorProperty =
+            DependencyProperty.Register(nameof(BookAuthor), typeof(string), typeof(ShoppingCartItem));
+    }
+}
