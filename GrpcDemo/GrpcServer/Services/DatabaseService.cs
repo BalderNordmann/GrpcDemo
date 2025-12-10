4584e7fccca66aa01c076d011a2962795c9b7a4a
BalderNordmann <zschornack.max@gmail.com>
2025-12-10 21:07:18 +0100

    Add DatabaseService, Add Nuget Microsoft.Data.Sqlite

---
+++ GrpcDemo/GrpcServer/Services/DatabaseService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace GrpcServer.Services
+{
+    public sealed class DatabaseService
+    {
+        private static readonly Lazy<DatabaseService> _instance =
+            new Lazy<DatabaseService>(() => new DatabaseService());
+
+        public static DatabaseService Instance => _instance.Value;
+
+        private bool disposed = false;
+
+        //private
+
+        private DatabaseService()
+        {
+            string connectionString = "Data Source=./Database/GrpcDemoDataBase.db";
+
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        ~DatabaseService()
+        {
+            Dispose();
+        }
+    }
+}
